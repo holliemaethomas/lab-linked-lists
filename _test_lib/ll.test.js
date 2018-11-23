17c009f4f4957ce48a7d65fc1e3f2f8b08de2de8
holliemaethomas <holliemaethomas85@gmail.com>
2018-11-23 13:51:24 -0800

    working

---
+++ _test_lib/ll.test.js
@@ -0,0 +1,42 @@
+
+'use strict';
+
+let LL = require ('../lib/ll.js')
+
+describe('Linked List', () => {
+
+  it('constructor()', () => {
+    let list = new LL();
+    expect(list.head).toBeNull();
+  });
+  it('append()', () => {
+    let list = new LL();
+    let firstValue = 'First One';
+    list.append(firstValue);
+    expect(list.head.value).toEqual(firstValue);
+    let newValue = '';
+    list.append(newValue);
+    expect(list.head.value.).toEqual(firstValue);
+    let nextPlace = '';
+    list.append(nextPlace);
+
+  });
+  it('prepend()', () => {
+    let list = new LL();
+    let firstValue = 'first Value';
+    List.prepend(firstValue);
+    let nextValue = 'next Value';
+    list.prepend(nextValue);
+    expect(nextValue).toEqual(list.head.value);
+  }); 
+  
+  it('insert after()', () => {
+    let list = new LL();
+    let value = Number;
+    let newValue = Number;
+    list.insertAfter(value, newValue);
+    expect(list.head.value).toEqual(newValue);
+  })
+
+  
+});
