414637184f10c447c01f606c270432ff46a3eb12
holliemaethomas <holliemaethomas85@gmail.com>
2018-11-23 15:55:18 -0800

    travis

--- _test_lib/ll.test.js
+++ _test_lib/ll.test.js
@@ -29,6 +29,17 @@ describe('Linked List', () => {
     list.prepend(nextValue);
     expect(nextValue).toEqual(list.head.value);
   }); 
+
+  it('reverse()', () => {
+    let list = new LL();
+    expect(curr.next).toEqual(prev); 
+  });
+
+  it('remove()', () => {
+    let list = new LL();
+    expect(value).toEqual(null);
+
+  })
   
  
 
